d929ee647f6154a5f20281fd12e9d82674c584ad
aziz-codes <azizisoffline@gmail.com>
2023-10-23 19:42:13 +0500

    setup the envoirnment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/components/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
